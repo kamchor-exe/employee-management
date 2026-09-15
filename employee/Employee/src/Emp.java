2445e4e088e048072d78b69ec8aaae0f158240c0
kamchor-exe <108235287+kamchor-exe@users.noreply.github.com>
2026-09-15 14:59:10 +0530

    Add employee management operations

--- employee/Employee/src/Emp.java
+++ employee/Employee/src/Emp.java
@@ -5,20 +5,22 @@ class Employee{
     public static int empId;
     public static String empName;
     public static String dept;
-    public static double salary;
+    public static int salary;
 }
 class Emp {
     private static final String url = "jdbc:postgresql://localhost:2007/employee";
     private static final String username = "postgres";
     private static final String password = System.getenv("DB_PASSWORD");
+
+    //Function 1: to add employee details to the database
     public void add(){
         Scanner sc = new Scanner(System.in);
       
-     System.out.println("Enter id, name , dept, salary");
+     System.out.println("\nEnter id, name , dept, salary");
                  Employee.empId = sc.nextInt();
                  Employee.empName = sc.next();
                  Employee.dept = sc.next();
-                 Employee.salary = sc.nextDouble();
+                 Employee.salary = sc.nextInt();
                 String query = "insert into emp (emp_id,emp_name,dept,salary) values(?,?,?,?)";
                 try{
                     Connection con = DriverManager.getConnection(url,username,password);
@@ -40,11 +42,13 @@ class Emp {
 
     }
 
+    //Function 2: to view all employee details from the database
     public void view_all(){
         try {
             try (Connection con = DriverManager.getConnection(url, username, password);
                  Statement stmt = con.createStatement();
-                 ResultSet rs = stmt.executeQuery("SELECT * FROM emp ORDER BY emp_id")) {
+                 //*ResultSet* is a Java object that stores the data returned from a relational database after executing an SQL query
+                 ResultSet rs = stmt.executeQuery("SELECT * FROM emp ORDER BY emp_id ASC")) {
 
                 // System.out.println("Connection Established\n");
 
@@ -66,14 +70,198 @@ class Emp {
         }        
     }
 
+    //Function 3: to Search employee details using emp_id
+    public void search(){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the id to be searched: ");
+        int id;
+        id=sc.nextInt();
+        try{
+            //connect and find the data
+            Connection con= DriverManager.getConnection(url,username,password);
+            String query= "select * from emp where emp_id= ?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, id);
 
+            //Display the data
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                System.out.println("\nEmployee with id: "+id);
+                System.out.println("\nID: "+rs.getInt("emp_id"));
+                System.out.println("Name: "+rs.getString("emp_name"));
+                System.out.println("Department: "+rs.getString("dept"));
+                System.out.println("Salary: "+rs.getInt("salary"));
+                System.out.println();
+            }
+            else{
+                System.out.println("ID not found!");
+            }
+        
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //Function 4: to update Name/Department/Salary (once at a time)
+    public void update(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter ID: ");
+        int id = sc.nextInt();
+        System.out.println("What to update?\n1:Name | 2:Department | 3:Salary");
+        int ch = sc.nextInt();
+
+
+
+        switch (ch) {
+            case 1://Update name
+               try{
+                System.out.println("Enter new name: ");
+                String name= sc.next();
+                Connection con = DriverManager.getConnection(url,username,password);
+                String query = "update emp set emp_name= ? where emp_id=?";
+                PreparedStatement pst = con.prepareStatement(query);
+                pst.setString(1, name);
+                pst.setInt(2, id);
+                pst.executeUpdate();
+                pst.close();
+                con.close();
+
+
+               }
+               catch(Exception e)
+               {
+                e.printStackTrace();
+               }
+
+                break;
+            
+            case 2://Update Department
+               try{
+                System.out.println("Enter new department: ");
+                String dept= sc.next();
+                Connection con = DriverManager.getConnection(url,username,password);
+                String query = "update emp set dept= ? where emp_id=?";
+                PreparedStatement pst = con.prepareStatement(query);
+                pst.setString(1, dept);
+                pst.setInt(2, id);
+                pst.executeUpdate();
+                pst.close();
+                con.close();
+
+
+               }
+               catch(Exception e)
+               {
+                e.printStackTrace();
+               }
+
+               break;
+
+            case 3://Update salary
+                try{
+                System.out.println("Enter new salary: ");
+                int sal = sc.nextInt();
+                Connection con = DriverManager.getConnection(url,username,password);
+                String query = "update emp set salary= ? where emp_id=?";
+                PreparedStatement pst = con.prepareStatement(query);
+                pst.setInt(1, sal);
+                pst.setInt(2, id);
+                pst.executeUpdate();
+                pst.close();
+                con.close();
+
+
+               }
+               catch(Exception e)
+               {
+                e.printStackTrace();
+               }
+
+               break;
+
+            default:
+                break;
+        }
+    }
+
+    //Function  5: to delete employee records. 
+    // Id of eleminated employee is lost until its assigned to another employee
+    public void delete(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter ID to be deleted: ");
+        int id = sc.nextInt();
+
+        try{
+            Connection con = DriverManager.getConnection(url,username,password);
+            String query= "delete from emp where emp_id=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, id);
+            pst.executeUpdate();
+            pst.close();
+            con.close();
+            System.out.println("Employee Eliminated Succesfully! :(");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void report(){
+        System.out.println("\n------------------------------");
+        System.out.println("Employee Report");
+        System.out.println("------------------------------\n");
+        
+        try {
+            Connection con = DriverManager.getConnection(url,username,password);
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select count(emp_id) from emp;");
+            if(rs.next()){
+                System.out.println("Total Employees: "+rs.getInt("count"));
+            }
+            
+            ResultSet rs1 = stmt.executeQuery("select max(salary) from emp;");
+            if(rs1.next()){
+                System.out.println("Highest Salary: "+ rs1.getInt("max"));
+            }
+            
+            ResultSet rs2 = stmt.executeQuery("select min(salary) from emp;");
+            if(rs2.next()){
+                System.out.println("Minimum Salary: "+ rs2.getInt("min"));
+            }
+
+            ResultSet rs3 = stmt.executeQuery("select avg(salary) from emp;");
+            if (rs3.next()) {
+                System.out.println("Average Saalry: "+rs3.getDouble("avg"));
+            }
+
+            ResultSet rs4 = stmt.executeQuery("select dept,count(emp_id) from emp group by dept;");
+            System.out.println("\n-----------------------------------");
+            System.out.println(" Employees In Each Department");
+            System.out.println("-----------------------------------\n");
+            while (rs4.next())
+            {
+
+                System.out.println(rs4.getString("dept")+ ":" +rs4.getInt("count")  );
+            }
+            System.out.println("");
+
+    }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+    }
+
+
+//Main function
     public static void main(String[] args) {
         Emp emp = new Emp();
         Scanner sc = new Scanner(System.in);
-        // int id;
-        // String name;
-        // String dept;
-        // double sal;
         int ch;
 
         if (password == null || password.isBlank()) {
@@ -88,7 +276,7 @@ class Emp {
             return;
         }
 
-        System.out.println("Choose the option:\n1.Add\n2.View All");
+        System.out.println("Choose the option:\n1.Add\n2.View All\n3.Search By ID\n4.Update\n5.Delete\n6.Report");
         System.out.print("Enter your choice: ");
         ch = sc.nextInt();
         switch (ch) {
@@ -98,6 +286,18 @@ class Emp {
             case 2:
                     emp.view_all();
                 break;
+            case 3: 
+                    emp.search();
+                    break;
+            case 4:
+                    emp.update();
+                    break;
+            case 5:
+                    emp.delete();
+                    break;
+            case 6:
+                    emp.report();
+                    break;
          }
 
 
